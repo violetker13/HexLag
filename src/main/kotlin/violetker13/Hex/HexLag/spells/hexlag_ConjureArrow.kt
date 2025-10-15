a8d52bd2b9f7e0acf9be498f7cca37167e4b69a4
violetker13 <igryvioletker@gmail.com>
2025-10-15 18:04:28 +0300

    1.1.7 YA PIDOR

--- src/main/kotlin/violetker13/Hex/HexLag/spells/hexlag_ConjureArrow.kt
+++ src/main/kotlin/violetker13/Hex/HexLag/spells/hexlag_ConjureArrow.kt
@@ -1,37 +1,27 @@
 package violetker13.Hex.HexLag.spells
 
-import at.petrak.hexcasting.api.casting.ParticleSpray
-import at.petrak.hexcasting.api.casting.RenderedSpell
+
 import at.petrak.hexcasting.api.casting.castables.Action
-import at.petrak.hexcasting.api.casting.castables.SpellAction
+
 import at.petrak.hexcasting.api.casting.eval.CastingEnvironment
 import at.petrak.hexcasting.api.casting.eval.OperationResult
 import at.petrak.hexcasting.api.casting.eval.env.PlayerBasedCastEnv
-import at.petrak.hexcasting.api.casting.eval.sideeffects.OperatorSideEffect
 import at.petrak.hexcasting.api.casting.eval.vm.CastingImage
 import at.petrak.hexcasting.api.casting.eval.vm.SpellContinuation
-import at.petrak.hexcasting.api.casting.getVec3
 import at.petrak.hexcasting.api.casting.iota.EntityIota
-import at.petrak.hexcasting.api.casting.iota.Iota
-import at.petrak.hexcasting.api.casting.iota.IotaType
 import at.petrak.hexcasting.api.casting.iota.Vec3Iota
 import at.petrak.hexcasting.api.casting.mishaps.MishapBadCaster
 import at.petrak.hexcasting.api.casting.mishaps.MishapInvalidIota
 import at.petrak.hexcasting.api.casting.mishaps.MishapNotEnoughArgs
 import at.petrak.hexcasting.api.casting.mishaps.MishapNotEnoughMedia
 import at.petrak.hexcasting.api.misc.MediaConstants
-import at.petrak.hexcasting.common.casting.actions.eval.OpEval
-import at.petrak.hexcasting.common.lib.HexParticles
 import at.petrak.hexcasting.common.lib.hex.HexEvalSounds
-import at.petrak.hexcasting.common.lib.hex.HexIotaTypes
-import net.minecraft.entity.Entity
-import net.minecraft.entity.projectile.ArrowEntity
+
 import net.minecraft.entity.projectile.PersistentProjectileEntity
 import net.minecraft.server.network.ServerPlayerEntity
 import violetker13.Hex.HexLag.Entities.HexxyArrowEntity
 
-import java.util.concurrent.Executors
-import java.util.concurrent.TimeUnit
+
 
 class hexlag_ConjureArrow : Action {
     override fun operate(
